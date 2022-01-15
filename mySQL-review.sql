edd1cd78a286bfbef440b4b639377d17c77a744d
adam-talbot <adam.talbot527@gmail.com>
2022-01-14 20:41:03 -0700

    SQL review

---
+++ mySQL-review.sql
@@ -0,0 +1,57 @@
+### MYSQL REVIEW ###
+
+USE employees;
+
+SHOW tables;
+
+DESCRIBE employees;
+
+SELECT 
+	CONCAT(first_name, ' ', last_name) as full_name,
+	birth_date, 
+	hire_date,
+	(DATEDIFF(curdate(), hire_date)) as number_of_days_employed
+FROM employees
+WHERE hire_date LIKE '199%'
+	AND birth_date LIKE '%-12-25'
+ORDER BY number_of_days_employed DESC;
+
+SELECT CONCAT(
+		LOWER(SUBSTR(first_name, 1, 1)), # lower-case first letter of first name 
+		LOWER(SUBSTR(last_name, 1, 4)), # lower-case first four letters of last name
+		'_', # add underscore
+		SUBSTR(birth_date, 6, 2), # month born
+		SUBSTR(YEAR(birth_date), 3, 2) # last 2 digits of year born
+		     ) as username, #create alias
+		   first_name, last_name, birth_date
+FROM employees;
+
+SELECT CONCAT(
+		LOWER(SUBSTR(first_name, 1, 1)), # lower-case first letter of first name 
+		LOWER(SUBSTR(last_name, 1, 4)), # lower-case first four letters of last name
+		'_', # add underscore
+		SUBSTR(birth_date, 6, 2), # month born
+		SUBSTR(birth_date, 3, 2) # last 2 digits of year born
+		     ) as username, #create alias
+		   first_name, last_name, birth_date
+FROM employees;
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%e'
+GROUP BY first_name, last_name;
+
+SELECT CONCAT(first_name, ' ', last_name) as full_name, COUNT(*) as n_same_name
+FROM employees
+WHERE last_name LIKE 'E%e'
+GROUP BY full_name
+HAVING n_same_name > 1;
+
+SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager' #select columns to be displayed and use aliases to rename
+FROM employees AS e #select starting table that we will be joining to and rename to facilitate referencing columns
+JOIN dept_manager AS dm #join with inner join to the employees the dept_manager table (associative table) renaming using an alias again
+ON dm.emp_no = e.emp_no #we are using column in common emp_no to connect the two tables
+JOIN departments AS d #join with inner join to the table the departments table renaming using alias again
+ON d.dept_no = dm.dept_no #we are using column in common (dept_no) to connect the two tables
+WHERE dm.to_date > curdate() AND e.gender = 'F' #Set condition to make sure we are only getting back current managers that are female
+ORDER BY d.dept_name; #order results to match order shown in exercises - can reference column name using original name or aliases
