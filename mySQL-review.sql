ee59291594392c305fc01383731e36372921b1c6
adam-talbot <adam.talbot527@gmail.com>
2022-01-25 11:51:18 -0700

    finished all mandatory SQL problems except one

--- mySQL-review.sql
+++ mySQL-review.sql
@@ -100,3 +100,70 @@ SELECT department_name
 FROM department
 WHERE department_block_number = 3
 ORDER BY department_name;
+
+SELECT partner_id, partner_name, phone_no
+FROM delivery_partners
+WHERE rating BETWEEN 3 AND 5
+ORDER BY partner_id;
+
+SELECT car_id, car_name, owner_id
+FROM cars
+WHERE car_type IN ('Hatchback', 'SUV')
+ORDER BY car_id;
+
+SELECT hotel_id, hotel_name, rating
+FROM hotel_details
+WHERE hotel_id IN
+    (SELECT DISTINCT hotel_id
+    FROM orders
+    WHERE order_date LIKE '%07%')
+ORDER BY hotel_id;
+
+SELECT student_name, department_name
+FROM student
+JOIN department using(department_id)
+WHERE city = 'Coimbatore'
+ORDER BY student_name;
+
+SELECT CONCAT(address, ', ', city) AS Address
+FROM student
+ORDER BY Address DESC;
+
+SELECT NAME, CONCAT(SUBSTR(NAME, 1, 3), SUBSTR(PHNO, 1, 3)) AS PASSWORD
+FROM USERS
+ORDER BY NAME;
+
+SELECT rental_id, car_id, customer_id, km_driven
+FROM rentals
+WHERE pickup_date LIKE '2019-08%';
+
+INSERT INTO rentals(rental_id, customer_id, car_id, pickup_date, return_date, km_driven, fare_amount)
+VALUES
+    ('R001', 'C007', 'V004', '2018-03-10', '2018-03-10', 800, 9000),
+    ('R002', 'C001', 'V007', '2018-03-11', '2018-03-12', 200, 3000),
+    ('R003', 'C007', 'V003', '2018-04-15', '2018-04-15', 100, 1500),
+    ('R004', 'C007', 'V001', '2018-05-16', '2018-05-18', 1000, 10000),
+    ('R005', 'C004', 'V005', '2018-05-10', '2018-05-12', 900, 11000),
+    ('R006', 'C004', 'V006', '2018-05-20', '2018-05-21', 200, 2500);
+    
+UPDATE customers SET phone_no = '9876543210' WHERE customer_id = 'CUST1004';
+
+SELECT customer_id, customer_name, address, phone_no
+FROM customers
+WHERE email_id LIKE '%@gmail.com'
+ORDER BY customer_id;
+
+SELECT car_id, car_name, car_type
+FROM cars
+WHERE car_name LIKE 'Maruthi%'
+    AND car_type = 'Sedan'
+ORDER BY car_id;
+
+SELECT CONCAT(hotel_name, ' is a ', hotel_type, ' hotel') AS HOTEL_INFO
+FROM hotel_details
+ORDER BY HOTEL_INFO DESC;
+
+/* What I want to do is check if the user has booked at a specific hotel once, if they have, they should be removed from list of users to display
+In python, you could loop through and create lists of all users and hotels and then if the hotel was in the list, remove them or add all others
+to another list and then filter by that final list, not sure how to do it in SQL */
+
